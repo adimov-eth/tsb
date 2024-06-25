a689d1d5ed73d082696d79674253ac561786be6e
Emil Karamanov <karamanov997@gmail.com>
2024-06-25 15:28:10 +0300

    init commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,49 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import {Bot, Context, webhookCallback} from "grammy";
+
+export interface Env {
+	TSTARS_SESSION: KVNamespace;
+	TSTARS_LEADERS: KVNamespace;
+	BOT_INFO: string;
+	BOT_TOKEN: string;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const bot = new Bot(env.BOT_TOKEN, {botInfo: JSON.parse(env.BOT_INFO)});
+
+		bot.command("start", async (ctx: Context) => {
+			if (!ctx.from || !ctx.from.id) {
+				await ctx.reply("Error: Unable to retrieve user information.");
+				return;
+			}
+
+			const userId = ctx.from.id.toString();
+
+			const existingData = await env.TSTARS_SESSION.get(userId);
+			let userData = existingData ? JSON.parse(existingData) : {};
+
+			userData = {
+				...userData,
+				...ctx.from
+			};
+
+			const photos = await bot.api.getUserProfilePhotos(ctx.from.id);
+
+			if (photos.total_count > 0) {
+				const photo = photos.photos[0][0];
+				const fileId = photo.file_id;
+
+				const file = await bot.api.getFile(fileId);
+				const fileUrl = `https://api.telegram.org/file/bot${env.BOT_TOKEN}/${file.file_path}`;
+
+				userData.photo_url = fileUrl;
+
+				await ctx.reply(`Hello!`);
+			}
+
+			await env.TSTARS_SESSION.put(userId, JSON.stringify(userData));
+		});
+
+		return webhookCallback(bot, "cloudflare-mod")(request);
 	},
-} satisfies ExportedHandler<Env>;
+};
